71999c1a7fc39973272ba52f50624eed72e94392
1mpress1ve <55937878+1mpress1ve@users.noreply.github.com>
2021-04-21 19:38:38 +0700

    Add files via upload

--- project/4.py
+++ project/4.py
@@ -1,23 +1,49 @@
 import random
+from tkinter import *
 
+window = Tk()
+window.title("Задание 4")
+window.geometry('800x500')
 
 x = random.randint(2,255)
 
-print('Напишите маску сети, в которой может быть до ',x,' компьютеров:')
+s = ''
+s = str(x)
 
-otvet = input()
+otvet = ''
+def clicked():
+	global otvet
+	res = format(txt.get())
+	otvet = res
+	k = otvet.count('.')
+	if k != 3:
+		lbl = Label(window, text="Неверно(")  
+		lbl.grid(column=0, row=4)
+	
+	else:
+		massiv = otvet.split('.')
+		if massiv[0] != '255' or massiv[1] != '255' or massiv[2] != '255':
+			lbl = Label(window, text="Неверно(")  
+			lbl.grid(column=0, row=4)
+		
+		else: 
+			if int(massiv[3]) != (254-x):
+				lbl = Label(window, text="Неверно(")  
+				lbl.grid(column=0, row=4)
+			
+			else:
+				lbl = Label(window, text=" Верно!!! ")  
+				lbl.grid(column=0, row=4)
 
-#   3 штучки 3 точки и еще одна правильная штучка которая 254-x
+lbl = Label(window, text="Напишите маску сети, в которой может быть до") 
+lbl.grid(column=0, row=0) 
+lbl = Label(window, text=s)  
+lbl.grid(column=1, row=0) 
+lbl = Label(window, text="компьютеров: ")  
+lbl.grid(column=2, row=0)
+txt = Entry(window,width=30)  
+txt.grid(column=0, row=1) 
+btn = Button(window, text="Ввести", command=clicked)  
+btn.grid(column=1, row=1)
 
-k = otvet.count('.')
-if k != 3:
-	print('Неверно(1')
-else:
-	massiv = otvet.split('.')
-	if massiv[0] != '255' or massiv[1] != '255' or massiv[2] != '255':
-		print('Неверно(2')
-	else: 
-		if int(massiv[3]) != (254-x):
-			print('Неверно(3')
-		else:
-			print('Верно!!') 
+window.mainloop()
